238fd7f3cc49d95394f527e189b9fb805e440d10
VitalyBo <108419833+VitalyBo@users.noreply.github.com>
2026-03-20 10:38:16 -0700

    still fixing

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-      },
-    ],
-  },
+  // Тут можна залишити порожньо або додати налаштування, якщо вони були
 };
 
 export default nextConfig;
